6f457df599de6ca59e1388b7116299a2b027c6dc
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-04-12 18:49:47 +0900

    author : JJH | message : history section 생성 responsive 시작

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// const historyCanvas = document.getElementById('history-canvas');
+// const ctx = historyCanvas.getContext('2d');
+// console.log(ctx);
+// ctx.lineWidth = 1.5;
+// ctx.fillStyle = '#ffffff';
+// ctx.strokeStyle = '#ffffff';
+// ctx.moveTo(0,20);
+// ctx.lineTo(2160, 20);
+// ctx.stroke();
